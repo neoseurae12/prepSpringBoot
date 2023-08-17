3780dff8a50520556dbdd0f586136587317d9687
Wonhui Roh <rwhui012@gmail.com>
2023-08-17 19:27:44 +0900

    Ch 1. Spring Boot 시작하기

---
+++ src/main/java/com/example/prepproject/PrepprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.prepproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrepprojectApplication.class, args);
+	}
+
+}
